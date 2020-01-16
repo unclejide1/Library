d73ad32da665cb1b2a71f2369d7885f185306d61
Jideofor Nwoke <njusticej@gmail.com>
2020-01-16 17:38:26 +0100

    add borrowers class to serve as a composition when creating the queue.

---
+++ src/com/decagonhq/Borrowers.java
@@ -0,0 +1,32 @@
+package com.decagonhq;
+
+public class Borrowers {
+    private String name;
+    private String bookTitle;
+
+    public Borrowers(String name, String bookTitle) {
+        this.name = name;
+        this.bookTitle = bookTitle;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBookTitle() {
+        return bookTitle;
+    }
+
+    public void setBookTitle(String bookTitle) {
+        this.bookTitle = bookTitle;
+    }
+
+    @Override
+    public String toString() {
+        return this.name + ": " + this.bookTitle;
+    }
+}
